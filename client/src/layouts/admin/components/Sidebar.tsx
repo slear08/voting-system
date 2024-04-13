5874c229fc9ab8a962053df0d0931d57c8e60c4c
slear08 <88441444+slear08@users.noreply.github.com>
2024-04-13 16:42:42 +0800

    feature: Added System Reset

--- client/src/layouts/admin/components/Sidebar.tsx
+++ client/src/layouts/admin/components/Sidebar.tsx
@@ -1,7 +1,18 @@
 import logo from '@/assets/logo.png';
 import { NavLink } from 'react-router-dom';
-import { LayoutDashboard, Bell, Users, ScrollText, Building2 } from 'lucide-react';
+import { LayoutDashboard, Bell, Users, ScrollText, Building2, RotateCcw } from 'lucide-react';
 import { Button } from '@/components/ui/button';
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger
+} from '@/components/ui/alert-dialog';
 
 const Sidebar = () => {
     return (
@@ -10,7 +21,7 @@ const Sidebar = () => {
                 <img src={logo} alt="logo" />
             </div>
             <div>
-                <div className="flex flex-col gap-40">
+                <div className="flex flex-col gap-20">
                     <div>
                         <div className="flex justify-center items-center gap-2 my-5">
                             <h1 className="font-bold">MENU</h1>
@@ -93,6 +104,36 @@ const Sidebar = () => {
                                 <Building2 />
                                 ORGANIZATIONS
                             </NavLink>
+                            <div className="flex justify-center items-center gap-2 my-5">
+                                <h1 className="font-bold">ADVANCE SETTINGS</h1>
+                                <span className="flex-1 h-[2px] bg-primary"></span>
+                            </div>
+                            <AlertDialog>
+                                <AlertDialogTrigger asChild>
+                                    <button className="flex items-center gap-2 p-2 font-semibold">
+                                        <RotateCcw />
+                                        RESTORE TO DEFAULT
+                                    </button>
+                                </AlertDialogTrigger>
+                                <AlertDialogContent>
+                                    <AlertDialogHeader>
+                                        <AlertDialogTitle>
+                                            Are you absolutely sure?
+                                        </AlertDialogTitle>
+                                        <AlertDialogDescription>
+                                            This action cannot be undone. This will permanently the
+                                            system restore to default state and current data will
+                                            delete also data from the servers.
+                                        </AlertDialogDescription>
+                                    </AlertDialogHeader>
+                                    <AlertDialogFooter>
+                                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                        <AlertDialogAction className="text-white">
+                                            Continue
+                                        </AlertDialogAction>
+                                    </AlertDialogFooter>
+                                </AlertDialogContent>
+                            </AlertDialog>
                         </div>
                     </div>
                     <div className="w-full">
